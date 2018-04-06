ca9fc425f357f85014c2ae95bdea945fe10b8c55
TheCoder87 <thecoder87@hotmail.com>
2018-04-06 19:54:19 +0530

    Resolved CA1032: Implement standard exception constructors issue

--- StaticCodeAnalysisManagedDemo/DemoException.cs
+++ StaticCodeAnalysisManagedDemo/DemoException.cs
@@ -1,9 +1,34 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace CodeAnalysisManagedDemo
 {
     public class DemoException : Exception
     {
+        public DemoException()
+            : base()
+        {
+
+        }
+
+        public DemoException(string s)
+            : base(s)
+        {
+
+        }
+
+        public DemoException(string s, Exception e)
+            : base(s, e)
+        {
+
+        }
+
+        public DemoException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+
+        }
+
         public static void Initialize(int size)
         {
 
