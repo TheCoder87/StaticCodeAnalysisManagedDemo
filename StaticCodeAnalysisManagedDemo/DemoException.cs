114d2aa9ff214830c8ba9554b5cb8170c10dab4a
TheCoder87 <thecoder87@hotmail.com>
2018-04-06 19:37:07 +0530

    Initial commit

---
+++ StaticCodeAnalysisManagedDemo/DemoException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CodeAnalysisManagedDemo
+{
+    public class DemoException : Exception
+    {
+        public static void Initialize(int size)
+        {
+
+        }
+
+        protected static readonly int _item;
+
+        public static int Item { get { return _item; } }
+    }
+}
